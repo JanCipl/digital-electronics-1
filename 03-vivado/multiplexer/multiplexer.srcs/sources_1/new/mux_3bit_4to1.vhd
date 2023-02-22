6186dc4d624fd02fa92849051ff4c63c4e8a35a8
Čipl Jan (240600) <240600@vutbr.cz>
2023-02-22 12:46:40 +0100

    adding files to PC 03

---
+++ 03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/22/2023 12:08:21 PM
+-- Design Name: 
+-- Module Name: mux_3bit_4to1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mux_3bit_4to1 is
+port(
+        b_i           : in  std_logic_vector(4 - 1 downto 0);
+        a_i           : in  std_logic_vector(4 - 1 downto 0);
+        c_i           : in  std_logic_vector(4 - 1 downto 0);
+        d_i           : in  std_logic_vector(4 - 1 downto 0);
+        f_o           : out std_logic
+        adr_i         : in std_logic_vector(4-1 downto 0);
+        );
+end  mux_3bit_4to1;
+
+architecture Behavioral of mux_3bit_4to1 is
+
+begin
+
+ f_o <= a_i when (addr_i = "000" ) else
+       b_i when (addr_i = "001" ) else
+       c_i when (addr_i = "010" ) else
+       d_i;                 -- All other combinations
+           
+
+end Behavioral;
