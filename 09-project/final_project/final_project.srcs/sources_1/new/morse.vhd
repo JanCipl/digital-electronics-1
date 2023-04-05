9f8552b390659a8d8b14e260b062f0b960a9f604
Čipl Jan (240600) <240600@vutbr.cz>
2023-04-05 12:42:53 +0200

    adding 7 seg to project

---
+++ 09-project/final_project/final_project.srcs/sources_1/new/morse.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/05/2023 11:28:09 AM
+-- Design Name: 
+-- Module Name: morse - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity morse is
+  Port ( 
+  tecka: in std_logic;
+  carka: in std_logic
+  );
+  
+end morse;
+
+architecture Behavioral of morse is
+
+begin
+
+
+
+
+end Behavioral;
